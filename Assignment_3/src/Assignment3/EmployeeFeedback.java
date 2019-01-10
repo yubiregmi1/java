06d1e7e8f27cfcab06560635e34b86f65558c04d
Yubi Regmi <yubiregmi@gmail.com>
2019-01-09 19:08:01 -0600

    Assignment 3 Employee Feedback

---
+++ Assignment_3/src/Assignment3/EmployeeFeedback.java
@@ -0,0 +1,15 @@
+package Assignment3;
+
+public class EmployeeFeedback {
+	
+	public static void main (String [] args) {
+		int [][]empInfo = {{1001,4,5}, {1002,2,4,5}};
+		
+		for (int outerLoop=0;outerLoop<empInfo.length;outerLoop++) { 
+			for (int innerLoop=0;innerLoop<empInfo[outerLoop].length;innerLoop++) {
+				System.out.println(empInfo[outerLoop][innerLoop]);
+			}
+		}
+	}
+
+}
