8778e3e79423a69714cbce1d3d265f9ae3b9a928
Yubi Regmi <yubiregmi@gmail.com>
2019-01-09 20:15:53 -0600

    Day 2 assignment 9

---
+++ Day2_9/src/Student.java
@@ -0,0 +1,45 @@
+public class Student
+{
+	private static int sID = 549;
+	private int studentId;
+	private char studentType;
+	private String studentName;
+	
+	Student()
+	{
+		studentId = sID; sID++;
+		studentType = 'F';
+		studentName = "test";
+	}
+	
+	Student(char sType, String fname, String lname)
+	{
+		studentId = sID; sID++;
+		studentType = sType;
+		studentName = fname+lname;
+	}
+	
+	public void displayDetails(Student std)
+	{
+		//System.out.println("ID : "+studentId);
+		//System.out.println("Type : "+studentType);
+		//System.out.println("Name : "+studentName);
+		System.out.println("ID : "+std.studentId);
+		System.out.println("Type : "+std.studentType);
+		System.out.println("Name : "+std.studentName);
+		
+	}
+	
+	public static void main(String args[])
+	{
+		Student displayValues = new Student();
+		Student studentOne = new Student('D',"Bony","Thomas");
+		//displayValues.displayDetails(studentOne);
+		studentOne.displayDetails(studentOne);
+		
+		Student studentTwo = new Student('H',"Dinil","Bose");
+		//displayValues.displayDetails(studentOne);
+		studentTwo.displayDetails(studentTwo);
+	}
+	
+}
