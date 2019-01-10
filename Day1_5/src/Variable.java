06a6bb5d1e670c0761aeb1da14ea6dc0e18514f1
Yubi Regmi <yubiregmi@gmail.com>
2019-01-09 20:11:44 -0600

    Day 1 assignment 5

---
+++ Day1_5/src/Variable.java
@@ -0,0 +1,16 @@
+class Variable
+{
+	public static void main (String args[])
+	{
+		int intVal = 5;
+		float floatVal = 250.5f;
+		double doubleVal = 2500.5;
+		boolean boolVal = true;
+		
+		System.out.println ("Integer\t:" + intVal + "\nFloat\t:" + floatVal +"\nDouble\t:" + doubleVal +"\nBoolean\t:" + boolVal);
+
+	}
+		
+}
+	
+
