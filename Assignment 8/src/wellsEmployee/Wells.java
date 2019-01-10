fbe6ca1ae4100e98546948f23398954ffa73ed34
Yubi Regmi <yubiregmi@gmail.com>
2019-01-09 19:09:42 -0600

    Employee grade

---
+++ Assignment 8/src/wellsEmployee/Wells.java
@@ -0,0 +1,33 @@
+package wellsEmployee;
+
+public class Wells {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		EmployeeGrade Elvis = new EmployeeGrade();
+		EmployeeGrade Martha = new EmployeeGrade();
+		Elvis.initializeEmployee(1, "Elivs", 4.1f, 3.9f,4.2f);
+		Martha.initializeEmployee(2, "Martha", 4.2f,4.4f);
+		
+		Elvis.calculateAverageFeedback();
+		Elvis.calculateGrade();
+		Elvis.displayInfo();
+		
+		Martha.calculateAverageFeedback();
+		Martha.calculateGrade();
+		Martha.displayInfo();
+		*/
+		//EmployeeGrade ram = new EmployeeGrade();
+		//ram.calculateAverageFeedback();
+		//ram.calculateGrade();
+		//ram.displayInfo();
+		
+		EmployeeGrade james = new EmployeeGrade(102,"James",4.2f,4.4f,4.1f);
+		james.calculateAverageFeedback();
+		james.calculateGrade();
+		james.displayInfo();
+
+	}
+
+}
